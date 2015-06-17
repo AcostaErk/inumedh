1b8f631ca2f65e756e83bd41c31e8a480d5dd47f
Erik Acosta <aerik_renato@hotmail.com>
2015-06-17 10:17:42 -0600

    Primer Commit del sitio

---
+++ Conexion/conn.php
@@ -0,0 +1,13 @@
+<?php
+$server = "localhost";
+$user = "root";
+$pass = "1234";
+$db = "bd_inumedh";
+
+/*$server = "localhost";
+$user = "root";
+$pass = "IproInfo1";
+$db = "iprovinay";*/
+?>
+
+
